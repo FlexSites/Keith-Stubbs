d94eb168e7fc8a69b97cd27c3885a934db9f5da7
Seth Tippetts <sethtippetts@gmail.com>
2015-05-31 20:33:05 -0600

    Adding first pass at mobile styling

--- source/js/contact.js
+++ source/js/contact.js
@@ -41,6 +41,18 @@ $(function(){
     $('#requiredMessage').show();
     event.preventDefault();
   }
+
+  var $testimonials = $('.testimonial');
+
+  var idx = 0;
+  function setActive(){
+    $('.testimonial_-active').removeClass('testimonial_-active');
+    if(idx >= $testimonials.length) idx = 0;
+    $testimonials.eq(idx++).addClass('testimonial_-active');
+    return setActive;
+  }
+
+  setInterval(setActive(), 7500);
 });
 
 
